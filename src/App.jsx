eaa31f68ba0c90286c51172faf44d2c36d68eb16
Fathe Allak <fatheallak7@gmail.com>
2025-10-31 02:41:39 +0300

    Add Full Project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './components/Header/Header.jsx'
+import Hero from './components/Hero/Hero.jsx'
+import Portfolio from './components/Portfolio/Portfolio.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import Footer from './components/Footer/Footer.jsx';
+
+function App(){
+
+  return <>
+    <Header />
+    <Hero />
+    <Portfolio />
+    <About />
+    <Contact />
+    <Footer />
+  </>
+}
+export default App;
